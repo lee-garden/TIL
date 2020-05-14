4dc08a3d38e1e4e41447c22dc068a142462b9817
GardenLee <khjvvv7@gmail.com>
2020-05-14 20:31:55 +0900

    jpa-basic ex01 - 3
    필드와 컬럼 매핑에 대한 공부

--- JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,46 +12,5 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
-
-        // JPA 의 모든 데이터 변경은 트랜잭션 안에서 일어나야 한다.
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        try {
-//            회원 등록
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HellloB");
-//
-//            em.persist(member);
-
-//            회원 조회
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
-
-//          JPQL
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-
-            // 영속성 컨텍스트의 detach, clear, close에 대한 이해
-            // detach = > 영속성 컨텍스트로의 분리
-            // clear = > 영속성 컨텍스트 비우기
-            // close = > 영속성 컨텍스트 종료
-            Member member1 = em.find(Member.class, 1L); // 쿼리 한번 나가고
-            em.clear();
-            Member member2 = em.find(Member.class, 1L); // 쿼리 한번 또나감
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
-//            em.close();
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-
-        emf.close();
     }
 }
