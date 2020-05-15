bd7dd9149597bcb6e120f31b23ba209d87a8dba3
GardenLee <khjvvv7@gmail.com>
2020-05-15 21:52:11 +0900

    jpa-basic ex01 - 5
    - 엔터티 구성 연습

--- JPA-basic/jpabook/src/main/java/jpabook/jpashop/domain/Item.java
+++ JPA-basic/jpabook/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,4 +1,50 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String anem;
+
+    private Integer price;
+
+    private Integer stockQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAnem() {
+        return anem;
+    }
+
+    public void setAnem(String anem) {
+        this.anem = anem;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
