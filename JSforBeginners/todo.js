ec285dc2fd8b6077ea12b3a57ae3aa75d98c5b8b
GardenLee <khjvvv7@gmail.com>
2020-05-17 01:22:01 +0900

    [JSMomentum] - To Do 만들기 part 1

---
+++ JSforBeginners/todo.js
@@ -0,0 +1,39 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintTodo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = "❌";
+
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+
+    paintTodo(currentValue);
+    toDoInput.value = "";
+}
+
+function loatToDos() {
+    const toDos = localStorage.getItem(TODOS_LS);
+    if (toDos !== null){
+
+    }
+ }
+
+function init(){
+    loatToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
