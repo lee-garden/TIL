d908cffa7f78e6ac65889bc3d97478c60eb62ceb
GardenLee <khjvvv7@gmail.com>
2020-05-19 23:16:27 +0900

    [JSMomentum] = Geo location api

--- JSforBeginners/weather.js
+++ JSforBeginners/weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = 'cfb3b9813c0b7484f3e348497ab68b28';
 const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +27,7 @@ function handleGeoSuccess(position) {
         longitude,
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,9 +43,11 @@ function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS);
 
     if (loadedCoords === null) {
-            askForCoords();
+        askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+        console.log(parseCoords);
     }
 }
 
