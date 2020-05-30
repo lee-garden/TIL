e581b97e03cab6a8fe3f81b2497fac8c800ab1f4
GardenLee <khjvvv7@gmail.com>
2020-05-31 01:11:01 +0900

    jpashop - 일대다 연관관계 매핑 실습

--- JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Team;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,7 +18,21 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setNaem("hello");
+
+            em.persist(member);
+
+            Team team = new Team();
+            team.setName("team1");
+
+            System.out.println("==================");
+            team.getMembers().add(member);
+            em.persist(team);
+            System.out.println("==================");
+            System.out.println("커밋 시작");
             tx.commit();
+            System.out.println("커밋 완료");
         } catch (Exception e) {
             tx.rollback();
         } finally {
