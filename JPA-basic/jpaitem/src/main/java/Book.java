aed7598bf6875ef24503efbba70112093d23ede0
GardenLee <khjvvv7@gmail.com>
2020-06-10 23:14:18 +0900

    jpa inheritance - 조인 전략 실습

---
+++ JPA-basic/jpaitem/src/main/java/Book.java
@@ -0,0 +1,8 @@
+import javax.persistence.Entity;
+
+@Entity
+public class Book extends Item {
+
+    private String author;
+    private String ibsn;
+}
---
+++ JPA-basic/jpaitem/src/main/java/JpaMain.java
@@ -0,0 +1,39 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Movie movie = new Movie();
+            movie.setDirector("A");
+            movie.setActor("BBB");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
+
+            em.persist(movie);
+            em.flush();
+            em.clear();
+
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println(findMovie);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
