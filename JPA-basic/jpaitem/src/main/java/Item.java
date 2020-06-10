eb34ceb45479aee979792c7f01827a89f3081252
GardenLee <khjvvv7@gmail.com>
2020-06-10 23:15:30 +0900

    jpa inheritance - 단일 테이블 전략 실습

--- JPA-basic/jpaitem/src/main/java/Item.java
+++ JPA-basic/jpaitem/src/main/java/Item.java
@@ -1,7 +1,7 @@
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
 public class Item {
 
