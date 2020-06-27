f049a746a219286cc4546271acc9c7d448550227
GardenLee <khjvvv7@gmail.com>
2020-06-27 15:19:25 +0900

    jpa - orphanRemoval에 대한 이해
    - orphanRemoval = > delete cascade

--- JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -26,9 +26,15 @@ public class JpaMain {
             parent.addChild(child1);
             parent.addChild(child2);
 
-            System.out.println("====================");
             em.persist(parent);
+
             em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
