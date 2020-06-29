32061b84fa94be0facc5ce0256d2d9926a2d435d
GardenLee <khjvvv7@gmail.com>
2020-06-30 00:20:52 +0900

    jpa - 값 타입 실전 예제

--- JPA-basic/jpabook/src/main/java/jpabook/jpashop/domain/Address.java
+++ JPA-basic/jpabook/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,13 +1,24 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
 
+    @Column(length = 20)
     private String city;
+
+    @Column(length = 20)
     private String street;
+
+    @Column(length = 5)
     private String zipCode;
+
+    public String fullAddress(){
+        return getCity() + getStreet() + " " + getZipCode();
+    }
 
     public Address(){}
 
@@ -21,7 +32,7 @@ public class Address {
         return city;
     }
 
-    public void setCity(String city) {
+    private void setCity(String city) {
         this.city = city;
     }
 
@@ -29,7 +40,7 @@ public class Address {
         return street;
     }
 
-    public void setStreet(String street) {
+    private void setStreet(String street) {
         this.street = street;
     }
 
@@ -37,7 +48,22 @@ public class Address {
         return zipCode;
     }
 
-    public void setZipCode(String zipCode) {
+    private void setZipCode(String zipCode) {
         this.zipCode = zipCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet()) &&
+                Objects.equals(getZipCode(), address.getZipCode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipCode());
     }
 }
