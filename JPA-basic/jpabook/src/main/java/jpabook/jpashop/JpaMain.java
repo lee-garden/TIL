6ddb51630ba9b2e47613003c766626960d434877
GardenLee <khjvvv7@gmail.com>
2020-07-03 13:46:07 +0900

    jpa - jpql 기본

--- JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,11 +21,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("hello");
-            member.setHomeAddress(new Address("city", "street", "100"));
+            List<Member> memberList = em.createQuery(
+                    "select m from Member m where m.name like '%kim%'",
+                    Member.class
+            ).getResultList();
 
-            em.persist(member);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
