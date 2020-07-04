b654892e3bd7e7d16c98942349ceb855fa57043a
JeongWon Liee <khjvvv7@gmail.com>
2020-07-04 20:19:19 +0900

    jpa - criteria 간략 실습 - criteria는 잘 사용하지 않으나 java에서 제공하는 기본 스펙이기 때문에 한번 둘러보는 정도로..

--- JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA-basic/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -9,6 +9,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JpaMain {
@@ -21,10 +24,23 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> memberList = em.createQuery(
-                    "select m from Member m where m.name like '%kim%'",
-                    Member.class
-            ).getResultList();
+
+            // Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> cq= query.select(m);
+
+            String username = "hello";
+
+            if (username != null) {
+                cq.where(cb.equal(m.get("name"), "kim"));
+            }
+
+            List<Member> resultList = em.createQuery(cq)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
