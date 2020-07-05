99fe966686ab853b9414ab4032f215770afc4f85
JeongWon Liee <khjvvv7@gmail.com>
2020-07-05 20:55:12 +0900

    jpa - jpql 기본문법 실습

---
+++ JPA-basic/hellojpql/src/main/java/jpql/Member.java
@@ -0,0 +1,43 @@
+package jpql;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private Integer age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
