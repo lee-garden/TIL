7f0357b9e72997a50aba42839ebe70a2a4d4315c
JeongWon Liee <khjvvv7@gmail.com>
2020-07-15 23:13:34 +0900

    jpashop - feat: 주문취소 컨트롤러 구현

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +27,7 @@ public class OrderService {
     /**
      * 주문
      * */
+    @Transactional
     public Long order(Long memberId, Long itemId, int count) {
 
         //엔티티 조회
@@ -50,6 +52,7 @@ public class OrderService {
     /**
      * 주문 취소
      * */
+    @Transactional
     public void cancelOrder(Long orderId) {
         Order order = orderRepository.findOne(orderId);
         order.cancel();
@@ -58,5 +61,7 @@ public class OrderService {
     /**
      * 주문 검색
      * */
-//    public List<Order> findOrders() {}
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll();
+    }
 }
