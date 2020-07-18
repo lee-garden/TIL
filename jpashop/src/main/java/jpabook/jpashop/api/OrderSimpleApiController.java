3233f2b51c2fbfd02abd4b6c16c90511f73ce500
JeongWon Liee <khjvvv7@gmail.com>
2020-07-18 20:19:35 +0900

    jpashop - feat: 주문 조회 API (V1)
    - 이렇게 하면 안된다.

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ * */
+@RequiredArgsConstructor
+@RestController
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> findOrders = orderRepository.findAll();
+        return findOrders;
+    }
+}
