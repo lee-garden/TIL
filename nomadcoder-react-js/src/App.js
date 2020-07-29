69e5ddb274c70e15b32dc7a2e5568c6ba0734f2e
JeongWon Liee <khjvvv7@gmail.com>
2020-07-29 22:33:57 +0900

    react - pros에 대한 이해
    - pros 동작 방식, 어떻게 넘겨 주는가

--- nomadcoder-react-js/src/App.js
+++ nomadcoder-react-js/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+function Food({ fav }) {
+return <h1>Hello {fav}</h1>
+}
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
+    </div>
+  );
 }
 
 export default App;
