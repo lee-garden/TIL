195c708df3941b50d6acb4bcdc2a4491e46ab1b8
JeongWon Liee <khjvvv7@gmail.com>
2020-09-20 14:39:42 +0900

    VanilaJS - Todo List 만들어 보기 (2)
    - 입력 받고 js로 DOM element 추가 하기

--- VanilaJS/simple-proj/todo/todo.js
+++ VanilaJS/simple-proj/todo/todo.js
@@ -1,2 +1,22 @@
 const inputBox = document.getElementById("inputBox");
 const content = document.getElementById("content");
+const todoUl = document.getElementById("todoListUl");
+
+function writeTodo(e) {
+  if (e.key === "Enter") {
+    console.log(inputBox.value);
+    saveTodo(inputBox.value);
+  }
+}
+
+function saveTodo(todoName) {
+  const todoItem = document.createElement("li");
+  todoItem.append(todoName);
+  todoUl.appendChild(todoItem);
+}
+
+function init() {
+  inputBox.addEventListener("keypress", writeTodo);
+}
+
+init();
