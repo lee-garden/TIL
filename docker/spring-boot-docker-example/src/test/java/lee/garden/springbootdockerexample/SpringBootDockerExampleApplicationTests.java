52bc700cffc2dd519f3fa484267addfe49220ab4
gardenLee <khjvvv7@gmail.com>
2021-05-22 03:31:46 +0900

    스프링 부트 도커 예제

---
+++ docker/spring-boot-docker-example/src/test/java/lee/garden/springbootdockerexample/SpringBootDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.garden.springbootdockerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
